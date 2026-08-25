2098d3a8e4fcf34cbc3af6f85e1a83ea05eac975
edariiida/darerq <100040706+darerq@users.noreply.github.com>
2026-08-25 12:30:34 +0300

    Add files via upload

---
+++ homework22/main.js
@@ -0,0 +1,21 @@
+const firstName = "Дарья";
+const lastName = "Мозговая";
+const isStudent = true;
+let age = 18;
+const currentYear = 2026;
+
+let birthYear = currentYear - age;
+
+console.log(
+	`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик/ученица курса: ${isStudent}`,
+);
+
+// Задача 4.
+// Какое значение будет у переменной result?
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+//123456789falsetrue
