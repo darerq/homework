15c2d9ebccacea3434ebed05ec471b53a6d19959
edariiida/darerq <100040706+darerq@users.noreply.github.com>
2026-09-25 13:11:42 +0300

    Add files via upload

---
+++ homework29/js/main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const todoKeys = {
+	id: "id",
+	text: "text",
+	is_completed: "is_completed",
+};
+
+const todos = [];
+
+const errTodoNotFound = todoId => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = todos =>
+	todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+	const newTodo = {
+		[todoKeys.id]: getNewTodoId(todos),
+		[todoKeys.text]: text,
+		[todoKeys.is_completed]: false,
+	};
+	todos.push(newTodo);
+	return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+	const todo = todos.find(todo => todo[todoKeys.id] === todoId);
+
+	if (!todo) {
+		console.error(errTodoNotFound(todoId));
+		return null;
+	}
+	todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+	return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+	const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId);
+	if (todoIndex === -1) {
+		console.error(errTodoNotFound(todoId));
+		return todos;
+	}
+	todos.splice(todoIndex, 1);
+	return todos;
+};
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+
+// Задача 1
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todosContain = document.querySelector(".todos");
+
+// Задача 2
+
+const createTodoElement = text => {
+	const todoElement = document.createElement("li");
+	todoElement.classList.add("todo");
+	todoElement.textContent = text;
+};
+
+// Задача 3
+
+const handleCreateTodo = (todos, text) => {
+	const newTodo = createTodo(todos, text);
+	const todoElement = createTodoElement(newTodo[todoKeys.text]);
+	todosContain.append(todoElement);
+};
